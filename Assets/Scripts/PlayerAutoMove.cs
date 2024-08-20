4fa3fc919ad480cdd424c5f8b07a02f2736da471
binmuxiz <leb001122@icloud.com>
2024-08-20 23:50:13 +0900

    자동 이동 오디오 추가

--- Assets/Scripts/PlayerAutoMove.cs
+++ Assets/Scripts/PlayerAutoMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerAutoMove : MonoBehaviour
 {
     public List<Transform> positionList = new List<Transform>();
+    public AudioSource audioSource;
     
     private float _moveSpeed = 2f;
     private int _nextIndex = 1;
@@ -24,13 +25,13 @@ public class PlayerAutoMove : MonoBehaviour
         {
             Vector3 targetPosition = positionList[_nextIndex].position;
             
-            // 플레이어의 현재 위치와 목표 위치 사이를 선형 보간하여 이동
             transform.position = Vector3.Lerp(transform.position, targetPosition, _moveSpeed * Time.deltaTime);
 
             // 목표 위치에 거의 도달했는지 확인
             if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
             {
                 _shouldMove = false; // 목표 위치에 도달하면 이동 중지
+                audioSource.Stop();
                 _nextIndex++;
             }
         }
@@ -40,5 +41,14 @@ public class PlayerAutoMove : MonoBehaviour
     public void StartMoving()
     {
         _shouldMove = true;
+        
+        // float journeyLength = Vector3.Distance(transform.position, positionList[_nextIndex].position);
+        // // 오디오 클립 길이 
+        // float clipLength = audioSource.clip.length;
+        // // 예상 이동 시간
+        // float travelTime = journeyLength / _moveSpeed;
+        // // 오디오 클립의 재생 속도를 이동 시간에 맞추기
+        // audioSource.pitch = clipLength / travelTime;
+        audioSource.Play();
     }
 }
